5a2d180aff831a0a0e175ae3a251f5dbde685340
sandiicemessanger <sandi@indochat.co.id>
2022-02-14 10:52:14 +0700

    login and redirect it into dashboard

---
+++ pages/login/index.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-5">
+                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" v-model="users.email" id="email" name="email" type="text" placeholder="entered your email" />
+                                <label for="inputEmail">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" v-model="users.password" id="password" name="password" type="password" placeholder="entered your password" />
+                                <label for="inputPassword">Password</label>
+                            </div>
+                            <div class="form-check mb-3">
+                                <input class="form-check-input" id="inputRememberPassword" type="checkbox" value="" />
+                                <label class="form-check-label" for="inputRememberPassword">Remember Password</label>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                <a class="small" href="password.html">Forgot Password?</a>
+                                <div class="d-grid"><button class="btn btn-primary btn-block" @click="store">Login</button></div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-footer text-center py-3">
+                        <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+  export default {
+
+    data() {
+      return {
+        //state post
+        users: {
+          email: '',
+          password: '',
+        },
+        //state validation
+        validation: []
+      }
+    },
+
+    methods: {
+      
+      //method "store"
+      async store(e) {
+        e.preventDefault()
+
+        //send data ke Rest API
+        await this.$axios.post('http://localhost:9595/api/v1/login', {
+            email: this.users.email,
+            password: this.users.password
+          })
+         
+
+          .then(() => {
+            
+            //redirect ke route "post"
+            this.$router.push({
+              name: 'category'
+            })
+
+          })
+          .catch(error => {
+            //assign validation  
+            this.validation = error.response.data
+            console.log(error)
+          })
+      }
+    }
+
+  }
+</script>
